fe79003e5722aabd8576f3dc08cd22a6e8a6119f
yuki takagi <yuki99takagi@gmail.com>
2020-05-21 15:18:16 +0900

    fix config.rbを修正

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module CustomerManagementApp
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.time.zone = "Tokyo"
+    config.time_zone = "Tokyo"
     config.i18n.load_path +=
       Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
